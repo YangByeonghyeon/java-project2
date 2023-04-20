df97a2f77a48ec759ab64457d60808ff6cddfa22
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-20 10:43:07 +0900

    feat: 입력받을 때 안내문 출력하기, Codeup - Codeup 1011, 1012 추가 - ScannerEx 변경

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1012.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+        System.out.println(String.format("%.6f", f));
+    }
+}

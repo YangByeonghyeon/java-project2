f80351d108627f7f260660ab8f2c063172107353
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-20 11:49:40 +0900

    feat: PrintfDate 사용법, Codeup 1014 - PrintfDate 추가 - Codeup 1014 추가

---
+++ src/main/java/com/example/javaproject2/week1/PrintfDate.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n", 4);
+        System.out.printf("%02d\n", 4);
+        System.out.printf("%02d\n", 11);
+        System.out.printf("%d-%02d-%d", 2023, 4, 20);
+    }
+}

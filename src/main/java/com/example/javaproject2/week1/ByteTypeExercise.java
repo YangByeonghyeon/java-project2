1cbaf2b116446791b8f36bbcfd65ee2cfbfba0f1
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-20 14:44:33 +0900

    feat: 변수가 필요한 경우 - ScannerFormatting 추가

---
+++ src/main/java/com/example/javaproject2/week1/ByteTypeExercise.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 127;
+        //byte byte2 = 128;
+        byte byte3 = -128;
+        //byte byte4 = -129;
+
+        System.out.println(byte1);
+        System.out.println(byte3);
+    }
+}

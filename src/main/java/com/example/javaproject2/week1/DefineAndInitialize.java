24607bf3d7a3391527e204c1f24e077c98b70a66
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-20 15:38:07 +0900

    feat: 변수에 담았던 값 출력하기 - DefineAndInitialize, ScannerAndVariable 추가

---
+++ src/main/java/com/example/javaproject2/week1/DefineAndInitialize.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        //int iVal = 1;
+        //PrintHello printHello = new PrintHello();
+        Scanner sc = new Scanner(System.in);
+        int iVal = sc.nextInt();
+        System.out.println(iVal);
+    }
+}

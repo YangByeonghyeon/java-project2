9ae37904add7643614089e47b688ac80420710d0
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-25 09:49:20 +0900

    feat: Codeup1034 ~ 1037 - Codeup1034 ~ 1037 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1037.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        char alphabet = (char)number;
+        System.out.println(alphabet);
+    }
+}

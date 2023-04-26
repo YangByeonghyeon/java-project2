387bf627a26648781574bce075d789257e2261cc
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-26 10:18:28 +0900

    feat: ClassInSchool, ClassInSchoolTest, Teacher - ClassInSchool 수정

--- src/main/java/com/example/javaproject2/week2/ClassInSchool.java
+++ src/main/java/com/example/javaproject2/week2/ClassInSchool.java
@@ -2,6 +2,6 @@ package com.example.javaproject2.week2;
 
 public class ClassInSchool {
     int no; // 초등: 1 ~ 2반 1반 ~ 10반
-    Teacher teacher = new Teacher();
-    Student[] students = new Student[30];
+    Teacher teacher;
+    Student[] students;
 }

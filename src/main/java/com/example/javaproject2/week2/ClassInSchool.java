5c883942698e515c5ae6512c47d8781d93b242aa
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-26 10:07:34 +0900

    feat: ClassInSchool, ClassInSchoolTest, Teacher - ClassInSchool, ClassInSchoolTest, Teacher 추가

---
+++ src/main/java/com/example/javaproject2/week2/ClassInSchool.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week2;
+
+public class ClassInSchool {
+    int no; // 초등: 1 ~ 2반 1반 ~ 10반
+    Teacher teacher = new Teacher();
+    Student[] students = new Student[30];
+}
---
+++ src/main/java/com/example/javaproject2/week2/ClassInSchoolTest.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week2;
+
+public class ClassInSchoolTest {
+    public static void main(String[] args) {
+        ClassInSchool classInSchool = new ClassInSchool();
+        classInSchool.no = 1;
+        classInSchool.teacher = new Teacher();
+        classInSchool.teacher.name = "양병현";
+        classInSchool.teacher.age = 29;
+        classInSchool.teacher.address = "서울 영등포구";
+        classInSchool.students = new Student[30];
+
+        System.out.println(classInSchool.teacher.name);
+        System.out.printf("%d반 담임 선생님 성함은 %s입니다.", classInSchool.no, classInSchool.teacher.name);
+    }
+}

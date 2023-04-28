58c8d4fb3985d3ee309340e113c0c876ea14042f
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-04-28 10:25:38 +0900

    feat: ifelse - IfWrong 추가

---
+++ src/main/java/com/example/javaproject2/week2/IfWrong.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week2;
+
+public class IfWrong {
+    public static void main(String[] args) {
+        int score = 81;
+        if (score >= 90) {
+            System.out.println("A");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}

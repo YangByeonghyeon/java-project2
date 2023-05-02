08755e69d3900c9fb1efb39bff88d9268a0a385b
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-02 14:11:52 +0900

    feat: While 예제 - While 예제

---
+++ src/main/java/com/example/javaproject2/week3/WhileEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3;
+
+public class WhileEx {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("Hello");
+        }
+    }
+}

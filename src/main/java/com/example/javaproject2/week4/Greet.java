b74de8f9dfee64ef1fd59a854859977eb4812eb6
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-08 11:45:08 +0900

    feat: 메소드 예제 - Greet, User, UserTest 추가

---
+++ src/main/java/com/example/javaproject2/week4/Greet.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week4;
+
+public class Greet {
+    public void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Greet greet = new Greet();
+        greet.printHello();
+    }
+}

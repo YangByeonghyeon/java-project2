3364b62fdfa36369a04740e36295a656fb428015
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-08 13:48:14 +0900

    feat: Greet, VariousReturnType, UserFactory - Greet, GreetTest 추가 - VariousReturnType 추가 - UserFactory, UserFactoryTest 추가

--- src/main/java/com/example/javaproject2/week4/GreetTest.java
+++ src/main/java/com/example/javaproject2/week4/GreetTest.java
@@ -1,12 +1,8 @@
 package com.example.javaproject2.week4;
 
-public class Greet {
-    private void printHello() {
-        System.out.println("Hello");
-    }
-
+public class GreetTest {
     public static void main(String[] args) {
         Greet greet = new Greet();
-        greet.printHello();
+        //greet.printHello();
     }
 }

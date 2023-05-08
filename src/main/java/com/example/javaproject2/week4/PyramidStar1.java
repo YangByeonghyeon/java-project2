8da0d16e92585d9b2e0289892114860c4df2e0a6
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-08 10:42:49 +0900

    feat: Codeup, 별 찍기 (피라미드) - Codeup1361 수정 - PyramidStar1 추가

--- src/main/java/com/example/javaproject2/week4/PyramidStar1.java
+++ src/main/java/com/example/javaproject2/week4/PyramidStar1.java
@@ -1,16 +1,19 @@
-package com.example.javaproject2.codeup;
+package com.example.javaproject2.week4;
 
 import java.util.Scanner;
 
-public class Codeup1361 {
+public class PyramidStar1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < num - i - 1; j++) {
                 System.out.printf(" ");
             }
-            System.out.println("**");
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.printf("*");
+            }
+                System.out.println();
         }
     }
 }

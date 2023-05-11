608bb9f71e46aa8e397872fe02173c098f1d6501
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-11 10:32:36 +0900

    feat: 리스트 예시, getter, setter - ListEx 변경 - GenericEx 추가 - User2 추가

--- src/main/java/com/example/javaproject2/week4/ListEX.java
+++ src/main/java/com/example/javaproject2/week4/ListEX.java
@@ -12,8 +12,13 @@ public class ListEX {
         System.out.println(l1.get(1));
         System.out.println(l1.size());
         System.out.println(l1.isEmpty());
-        l1.remove(0);
-        l1.remove(0);
+        /*l1.remove(0);
+        l1.remove(0);*/
         System.out.println(l1.isEmpty());
+
+        System.out.println("----------");
+        for(var item : l1) { // for each
+            System.out.println(item);
+        }
     }
 }
---
+++ src/main/java/com/example/javaproject2/week4/User2.java
@@ -0,0 +1,41 @@
+package com.example.javaproject2.week4;
+
+public class User2 {
+    private String name;
+    private String phoneNumber; // "01012345678" --> 0없어짐
+    private String passWord;
+    private int age;
+    public User2(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+    public String getPassWord() {
+        return passWord;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public int getAge() {
+        return age;
+    }
+    boolean isAdult() {
+        return age >= 18;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

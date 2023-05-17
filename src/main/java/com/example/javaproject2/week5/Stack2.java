07d1547a113178e1ec9db7f9c0e254c013f2c9ee
DESKTOP-L67RPGP\병현 <rkdalsfnql@naver.com>
2023-05-17 09:42:09 +0900

    feat: 스택 예제 pop - Stack1 변경 - Stack2 추가

--- src/main/java/com/example/javaproject2/week5/Stack2.java
+++ src/main/java/com/example/javaproject2/week5/Stack2.java
@@ -2,20 +2,28 @@ package com.example.javaproject2.week5;
 
 import java.util.Arrays;
 
-public class Stack1 {
+public class Stack2 {
     private int[] arr = new int[10000];
     private int pointer = 0;
     /*public Stack1(int size) {
         this.arr = new int[size];
     }*/
+    public int pop() {
+        /*int temp = this.arr[pointer - 1];
+        pointer--;
+        return temp;*/
+        return this.arr[--pointer]; //리팩토링
+    }
     public void push(int value) {
         this.arr[pointer++] = value;
         System.out.println(Arrays.toString(arr));
         System.out.println("pointer = " + pointer);
     }
     public static void main(String[] args) {
-        Stack1 st = new Stack1();
-        st.push(10);
-        st.push(20);
+        Stack2 st2 = new Stack2();
+        st2.push(10);
+        st2.push(20);
+        System.out.println(st2.pop());
+        System.out.println(st2.pop());
     }
 }
